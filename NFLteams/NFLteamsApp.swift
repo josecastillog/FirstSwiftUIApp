a2e844f2fc133b82f02b8cde4f2f0e4697599889
josecas <jmcg0901@gmail.com>
2022-09-27 13:49:20 -0500

    Initial Commit

---
+++ NFLteams/NFLteamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFLteamsApp.swift
+//  NFLteams
+//
+//  Created by Jose Castillo on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct NFLteamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
